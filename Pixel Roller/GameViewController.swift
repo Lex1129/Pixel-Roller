358a5b2afafb0cf59fe93c2ba33aaa05d5cd1f80
Lexicon <lexicon@tavernsavepoint.com>
2019-10-23 15:59:47 -0400

    0.1.2
    All D4 and D6 dice now contribute to the score in tandem.

--- Pixel Roller/GameViewController.swift
+++ Pixel Roller/GameViewController.swift
@@ -16,6 +16,7 @@ var randomD6Index = 0
 var diceSpotIndex = 0
 var totalPoints = 0
 var d4Points = 0
+var d6Points = 0
 
 
 
@@ -122,42 +123,49 @@ class GameViewController: UIViewController {
             rollD6()
             diceSpot2.image = UIImage(named: d6Array[randomD6Index])
             diceSpotIndex = 2
-            
+            totalPoints += d6Points
         case 2:
             rollD6()
             diceSpot3.image = UIImage(named: d6Array[randomD6Index])
             diceSpotIndex = 3
+            totalPoints += d6Points
         case 3:
             rollD6()
             diceSpot4.image = UIImage(named: d6Array[randomD6Index])
             diceSpotIndex = 4
+            totalPoints += d6Points
         case 4:
             rollD6()
             diceSpot5.image = UIImage(named: d6Array[randomD6Index])
             diceSpotIndex = 5
+            totalPoints += d6Points
         case 5:
             rollD6()
             diceSpot6.image = UIImage(named: d6Array[randomD6Index])
             diceSpotIndex = 6
+            totalPoints += d6Points
         case 6:
             rollD4()
             diceSpot7.image = UIImage(named: d6Array[randomD6Index])
             diceSpotIndex = 7
+            totalPoints += d6Points
         case 7:
             rollD4()
             diceSpot8.image = UIImage(named: d6Array[randomD6Index])
             diceSpotIndex = 8
+            totalPoints += d6Points
         case 8:
             rollD4()
             diceSpot9.image = UIImage(named: d6Array[randomD6Index])
             diceSpotIndex = 9
+            totalPoints += d6Points
         default:
             rollD6()
             diceSpot1.image = UIImage(named: d6Array[randomD6Index])
             diceSpotIndex = 1
-            
+            totalPoints += d6Points
         }
-
+        totalScore.text = "\(totalPoints)"
     }
     
 
@@ -183,6 +191,19 @@ class GameViewController: UIViewController {
     
         func rollD6() {
             randomD6Index = Int.random(in: 0 ... 5)
+            if randomD6Index == 0{
+                d6Points = 1
+            }else if randomD6Index == 1{
+                d6Points = 2
+            }else if randomD6Index == 2{
+                d6Points = 3
+            }else if randomD6Index == 3{
+                d6Points = 4
+            }else if randomD6Index == 4{
+                d6Points = 5
+            }else if randomD6Index == 5{
+                d6Points = 6
+            }
     }
     
         func reset() {
